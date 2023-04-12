73f484cf4fe74061547436166cff80e2f81f7720
nabin166 <nabinaryal830@gmail.com>
2023-04-12 15:48:17 +0545

    Fontawesome icon setup

--- src/Components/shared/Navigation/Navigation.jsx
+++ src/Components/shared/Navigation/Navigation.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+ import {faHand} from '@fortawesome/free-solid-svg-icons'
 
 const Navigation = () => {
   return (
-    <div>This is Navigation</div>
+    <nav>
+      
+        
+          <h1><Link to='/'><FontAwesomeIcon icon={faHand}></FontAwesomeIcon>hello</Link></h1>
+         
+    </nav>
   )
 }
 
