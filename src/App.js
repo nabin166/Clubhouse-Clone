01c9368c6b902affe8ebb850bc7904d350ad5e61
nabin166 <nabinaryal830@gmail.com>
2023-04-12 11:56:34 +0545

    Routing Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter,Routes ,Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Navigation from './Components/shared/Navigation/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      This is our homepage .
-    </div>
+    <BrowserRouter>
+    <Navigation/>
+      <Routes>
+        <Route path="/" element={<Home/>}>
+
+          
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
