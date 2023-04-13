0ac2ebe92681706db980d77a4f3d2236244c3655
nabin166 <nabinaryal830@gmail.com>
2023-04-13 07:47:13 +0545

    Navbar-component-setup

--- src/Components/shared/Navigation/Navigation.jsx
+++ src/Components/shared/Navigation/Navigation.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
- import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
- import {faHand} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHand } from '@fortawesome/free-solid-svg-icons'
+import styles from './Navigation.module.css'
 
 const Navigation = () => {
+  const Brandstyle = {
+    textDecoration:'none',
+    
+  }
+
   return (
-    <nav>
-      
-        
-          <h1><Link to='/'><FontAwesomeIcon icon={faHand}></FontAwesomeIcon>hello</Link></h1>
-         
+    <nav className='container'>
+
+      <Link to='/' style={Brandstyle} >
+        <div className={styles.flex}>
+          <img src="images/logo.png" alt="logo" />
+          <span className={styles.spans}>Clubhouse</span>
+        </div>
+      </Link>
+
     </nav>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <h1>This is homepage</h1>
+    <h1></h1>
   )
 }
 
